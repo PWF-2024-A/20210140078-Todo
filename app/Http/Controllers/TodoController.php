58c31631172e59d61cc73ab01e7383d9118a3b85
Dwiponco Suripto <dwiponcosuripto44@gmail.com>
2024-04-01 14:23:49 +0700

    create, read, search data

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,24 +3,79 @@
 namespace App\Http\Controllers;
 
 use App\Models\Todo;
+use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
-        $todos = Todo::where('user_id', auth()->user()->id)->get();
-        dd($todos);
+        $todos = Todo::where('user_id', auth()->user()->id)
+            ->orderBy('is_complete', 'asc')
+            ->orderBy('created_at', 'desc')
+            ->get();
 
-        return view('todo.index');
+        // dd($todos);
+
+        return view('todo.index', compact('todos'));
     }
 
+    /**
+     * Show the form for creating a new resource.
+     */
     public function create()
     {
         return view('todo.create');
     }
 
-    public function edit()
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, Todo $todo)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+
+        $todo = Todo::create([
+            'title' => ucfirst($request->title),
+            'user_id' => auth()->user()->id,
+        ]);
+
+        return redirect()->route('todo.index')->with('success', 'Todo created successfully!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Todo $todo)
+    {
+        //nampilin satu2
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Todo $todo)
     {
         return view('todo.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        //update sesuatu
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Todo $todo)
+    {
+        //hapus data
     }
 }
---
+++ tugas5.md
@@ -0,0 +1,17 @@
+# Tugas 5
+
+## 1. Todo Create
+
+![Alt text] (screenshot/tugas5/Screenshot 2024-04-01 141032.png)
+
+## 2. Todo Created
+
+![Alt text] (screenshot/tugas5/Screenshot 2024-04-01 141303.png)
+
+## 3. User Search
+
+![Alt text] (screenshot/tugas5/Screenshot 2024-04-01 141405.png)
+
+## 4. User Search Result
+
+![Alt text] (screenshot/tugas5/Screenshot 2024-04-01 141534.png)
