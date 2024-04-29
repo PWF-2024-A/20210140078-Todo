bfa855b847878d69d3292199b53dfb6b08cc1625
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2024-04-29 20:11:58 +0700

    middleware, gate admin

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\TodoController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,20 +26,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-    Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
-    Route::post('/todo', [TodoController::class, 'store'])->name('todo.store');
-    Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
-    Route::get('/todo/{todo}/edit', [TodoController::class, 'edit'])->name('todo.edit');
-    Route::patch('/todo/{todo}', [TodoController::class, 'update'])->name('todo.update');
-    Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
-    Route::patch('/todo/{todo}/incomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
-    Route::delete('/todo/{todo}', [TodoController::class, 'destroy'])->name('todo.destroy');
-    Route::delete('/todo', [TodoController::class, 'destroyCompleted'])->name('todo.deleteallcompleted');
+require __DIR__.'/auth.php';
+
+Route::middleware('admin')->group (function () {
     Route::get('/user', [UserController::class, 'index'])->name('user.index');
     Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
     Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
     Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
-});
-
-require __DIR__.'/auth.php';
+    });
---
+++ tugas7.md
@@ -0,0 +1,10 @@
+# Tugas 7
+
+## 1. Middleware & Gate Admin Not Active
+
+![Alt text](screenshot/tugas7/Screenshot%2024-04-29%195517.png)
+
+## 2. Middleware & Gate Admin Active
+
+![Alt text](screenshot/tugas7/Screenshot&2024-04-29&200207.png)
+
