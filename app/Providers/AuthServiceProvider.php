a765769cbcd51c5eb27f13a174859bdaef2b2eab
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2024-04-29 23:26:26 +0700

    middleware, gate admin

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -21,6 +21,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('admin', function (User $user) {
+            return $user->is_admin == true;
+        });
     }
 }
